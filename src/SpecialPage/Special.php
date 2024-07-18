339abcfcf8b344f76aaf887ddf0178edcc17710a
YvarRavy <ynanlohy@archixl.nl>
2024-07-18 12:16:37 +0200

    remove use of SMW HtmlTabs

--- src/SpecialPage/Special.php
+++ src/SpecialPage/Special.php
@@ -14,7 +14,6 @@ use MediaWiki\Extension\SmartComments\DBHandler;
 use MediaWiki\Extension\SmartComments\SemanticInlineComment as SIC;
 use MediaWiki\Extension\SmartComments\Settings\Handler;
 use SMW\DIWikiPage;
-use SMW\Utils\HtmlTabs;
 use SpecialPage;
 use Title;
 use Xml;
@@ -109,23 +108,18 @@ class Special extends SpecialPage {
 	private function showMain() : void {
 		$out = $this->getOutput();
 
-		$htmlTabs = new HtmlTabs();
-		$htmlTabs->setGroup( "smartcomments-special" );
-		$htmlTabs->setActiveTab( self::TAB_ID_OVERVIEW );
-
-		$htmlTabs->tab( self::TAB_ID_OVERVIEW, wfMessage( "sic-sp-tab-overview" )->text() );
-		$htmlTabs->content( self::TAB_ID_OVERVIEW, $this->showOverview() );
-
-		$htmlTabs->tab( self::TAB_ID_HELP, wfMessage( "sic-sp-tab-help" )->text() );
-		$htmlTabs->content( self::TAB_ID_HELP, $this->getHelpTab() );
-
-		$tabs = [ self::TAB_ID_OVERVIEW, self::TAB_ID_HELP ];
-		$inlineStyles = [];
-		foreach ( $tabs as $tabName ) {
-			$inlineStyles[] = ".smartcomments-special #tab-$tabName:checked ~ #tab-content-$tabName";
-		}
-		$out->addInlineStyle( implode( ',', $inlineStyles ) . ' {display: block;}' );
-		$out->addHTML( $htmlTabs->buildHTML( [ "class" => "smartcomments-special" ] ) );
+		$tabLabels = Xml::openElement( 'div', [ 'class' => 'sc-tabs' ] );
+		$tabLabels .= Xml::element( 'span', [ 'class' => 'sc-tab active', 'tab-reference' => "tab-" . self::TAB_ID_OVERVIEW ], wfMessage( "sic-sp-tab-overview" )->text() );
+		$tabLabels .= Xml::element( 'span', [ 'class' => 'sc-tab', 'tab-reference' => "tab-" . self::TAB_ID_HELP ], wfMessage( "sic-sp-tab-help" )->text() );
+		$tabLabels .= Xml::closeElement( 'div' );
+		$out->addhtml( $tabLabels );
+		$tabBody = Xml::openElement( "section", [ 'id' => 'tab-' . self::TAB_ID_OVERVIEW, 'class' => 'sc-tab-body hide-tab' ] );
+		$tabBody .= $this->showOverview();
+		$tabBody .= Xml::closeElement( "section" );
+		$tabBody .= Xml::openElement( "section", [ 'id' => 'tab-' . self::TAB_ID_HELP, 'class' => 'sc-tab-body hide-tab' ] );
+		$tabBody .= $this->getHelpTab();
+		$tabBody .= Xml::closeElement( "section" );
+		$out->addHTML( $tabBody );
 	}
 
 	/**
