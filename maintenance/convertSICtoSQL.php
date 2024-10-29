7789be84eeb970c291e87e6b060edb546604a86a
YvarRavy <ynanlohy@archixl.nl>
2024-10-29 12:59:30 +0100

    fix script

--- maintenance/convertSICtoSQL.php
+++ maintenance/convertSICtoSQL.php
@@ -24,57 +24,67 @@ class convertSICtoSQL extends Maintenance
 	 * Moves the last revision its content to the new created sic_diff_table
 	 */
 	private function moveLastRevisionToSIC() {
-		$tables = [
-			'wt' => 'text',
-			'ws' => 'slots',
-			'wsr' => 'slot_roles',
-			'wr' => 'revision',
-			'wp' => 'page',
-			'wsa' => DBHandler::DB_TABLE_SIC_ANCHOR
-		];
+		$slotRoleTable = 'slot_roles';
+		$slotsTable = 'slots';
+		$revisionTable = 'revision';
+		$pageTable = 'page';
 
-		$fields = [
-			'wp.page_id',
-			'wp.page_latest',
-			'wt.old_text'
-		];
-		$joins = [
-			'ws' => [ 'JOIN', 'wt.old_id = ws.slot_content_id' ],
-			'wsr' => [ 'JOIN', 'ws.slot_role_id = wsr.role_id' ],
-			'wr' => [ 'JOIN', 'ws.slot_revision_id = wr.rev_id' ],
-			'wp' => [ 'JOIN', 'wr.rev_id = wp.page_latest' ],
-			'wsa' => [ 'JOIN', 'wp.page_id = wsa.page_id' ]
-		];
-
-		$conditions = [
-			'wsr.role_name' => 'sic-data-slot'
-		];
-
-		$res = $this->dbr->select(
-			$tables,
-			$fields,
-			$conditions,
-			__METHOD__,
-			[
-				'GROUP BY' => [ 'wp.page_id', 'wp.page_latest', 'wt.old_text' ]
-			],
-			$joins
+		// Get the slot_role_id for 'sic-data-slot'
+		$slotRoleId = $this->dbr->selectField(
+			$slotRoleTable,
+			'role_id',
+			[ 'role_name' => 'sic-data-slot' ],
+			__METHOD__
 		);
 
-		$insertRows = [];
-		$page_ids = [];
-		foreach ( $res as $row ) {
-			$insertRows[] = [
-				'page_id' => $row->page_id,
-				'text' => $row->old_text
-			];
-			$page_ids[] = $row->page_id;
+		if (!$slotRoleId) {
+			$this->output("No slot role found for 'sic-data-slot'.\n");
+			return;
 		}
 
+		// Get all slots with the slot_role_id
+		$slots = $this->dbr->select(
+			$slotsTable,
+			'slot_revision_id',
+			[ 'slot_role_id' => $slotRoleId ],
+			__METHOD__
+		);
+
+		$revisionIds = [];
+		foreach ($slots as $slot) {
+			$revisionIds[] = $slot->slot_revision_id;
+		}
+
+		if (empty($revisionIds)) {
+			$this->output("No slots found for the given slot role.\n");
+			return;
+		}
+
+		// Get all unique pages from the revisions
+		$res = $this->dbr->select(
+			[ 'r' => $revisionTable, 'p' => $pageTable],
+			[ 'DISTINCT p.page_id', 'p.page_latest'],
+			[ 'r.rev_id' => $revisionIds ],
+			__METHOD__,
+			[],
+			[
+				'p' => [ 'JOIN', 'r.rev_page = p.page_id' ],
+			]
+		);
+
+		$rev_ids = [];
+		$page_ids = [];
+		foreach ($res as $row) {
+			$page = MediaWikiServices::getInstance()->getWikiPageFactory()->newFromID( $row->page_id );
+			$insertRows[ $row->page_latest ] = [
+				'page_id' => $row->page_id,
+				'text' => serialize($page->getRevisionRecord()->getContent( 'sic-data-slot' )->getText()),
+			];
+		}
 		if ( !empty( $insertRows ) ) {
 			$this->dbw->insert(
 				DBHandler::DB_TABLE_SIC_DIFF,
-				$insertRows,
+				array_values( $insertRows ),
 				__METHOD__
 			);
 
@@ -85,6 +95,8 @@ class convertSICtoSQL extends Maintenance
 	}
 
 	private function clearSicDataSlots() {
+		$this->dbw->startAtomic( __METHOD__ );
+		$this->dbr->startAtomic( __METHOD__ );
 		$slot_role = $this->dbr->tableName( 'slot_roles' );
 		$slots = $this->dbr->select(
 			[
@@ -115,6 +127,8 @@ class convertSICtoSQL extends Maintenance
 		$this->deleteFromContentTable( $output['slot_content_id'] );
 		$this->deleteFromTextTable( $output['slot_content_id'] );
 		$this->deleteFromSlotTables( $output['slot_role_id'] );
+		$this->dbw->endAtomic( __METHOD__ );
+		$this->dbr->endAtomic( __METHOD__ );
 		return true;
 	}
 
